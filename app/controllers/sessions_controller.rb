13bbb1d73666529d8478e8b9e3aa15d9f53d28a1
Georjane <pzgeorjane@gmail.com>
2020-10-14 13:22:48 +0000

    Add session helper methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    user = User.find_by(name: params[:session][:name.downcase)
+    if user && user.authenticate(params[:session][:name])
+    # Log the user in and redirect to the user's show page.
+    log_in user
+    redirect_to user
+    else
+    # Create an error message.
+      flash[:danger] = 'Invalid username'
+      render 'new'
+    end
+  end
+  
+  def destroy
+  end
 end
