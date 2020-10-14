1429ced9772f4909e636aa56717a788ae333c496
Georjane <pzgeorjane@gmail.com>
2020-10-14 14:49:41 +0000

    Include associations between user and article models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 140 }
+  validates :body, presence: true, length: { maximum: 500 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
+  has_many :articles, dependent: :destroy
   validates :name, presence: true, length: { maximum: 50 }
 end
