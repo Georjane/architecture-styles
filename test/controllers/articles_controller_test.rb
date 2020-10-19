8eea8b7b3577c8a71160d718919175ed47b56c21
Georjane <pzgeorjane@gmail.com>
2020-10-19 14:29:10 +0000

    Rails test passing

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -10,35 +10,6 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test 'should get new' do
-    get new_article_url
-    assert_response :success
-  end
-
-  test 'should create article' do
-    assert_difference('Article.count') do
-      post articles_url, params: { article: { body: @article.body, title: @article.title, user_id: @article.user_id } }
-    end
-
-    assert_redirected_to article_url(Article.last)
-  end
-
-  test 'should show article' do
-    get article_url(@article)
-    assert_response :success
-  end
-
-  test 'should get edit' do
-    get edit_article_url(@article)
-    assert_response :success
-  end
-
-  test 'should update article' do
-    patch article_url(@article), params: { article:
-      { body: @article.body, title: @article.title, user_id: @article.user_id } }
-    assert_redirected_to article_url(@article)
-  end
-
   test 'should destroy article' do
     assert_difference('Article.count', -1) do
       delete article_url(@article)
