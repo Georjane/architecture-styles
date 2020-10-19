c08c482673af1a8a8a04517d9bc670a21c1e9482
Georjane <pzgeorjane@gmail.com>
2020-10-19 11:35:08 +0000

    Add icons to footer and style login page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
   belongs_to :section
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :body, presence: true, length: { minimum: 20 }
   has_many :votes, dependent: :destroy
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
 
