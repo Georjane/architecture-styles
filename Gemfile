02e705e3ff14879235560b4db1133564aa4cf873
Georjane <pzgeorjane@gmail.com>
2020-10-19 13:42:27 +0000

    Change redirect path upon vote

--- Gemfile
+++ Gemfile
@@ -5,8 +5,7 @@ ruby '2.6.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.4'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
@@ -40,6 +39,8 @@ gem 'paperclip'
 gem 'bootsnap', '>= 1.1.0', require: false
 
 group :development, :test do
+  # Use sqlite3 as the database for Active Record
+  gem 'sqlite3'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,21 +3,17 @@ class VotesController < ApplicationController
   before_action :find_vote, only: [:destroy]
 
   def create
-    if already_voted?
-      flash[:notice] = "You can't vote more than once"
-    else
+    if !already_voted?
       @vote = current_user.votes.create(article_id: params[:article_id])
     end
-    redirect_to articles_path, notice: 'You voted article.'
+    redirect_to @article, notice: 'You voted this article.'
   end
 
   def destroy
-    if !already_voted?
-      flash[:notice] = 'Cannot unlike'
-    else
+    if already_voted?
       @vote.destroy
     end
-    redirect_to articles_path, notice: 'You unvoted an article.'
+    redirect_to @article, notice: 'You unvoted this article.'
   end
 
   private
