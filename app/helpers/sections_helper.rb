1ecbf632a51078b6683602c9274dd6506d21e489
Georjane <pzgeorjane@gmail.com>
2020-10-21 11:39:40 +0000

    Create sections_helper method

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -1,2 +1,22 @@
 module SectionsHelper
+
+  def display_row1_elements(articles)
+    count = 1
+    results = []
+    articles.each do |article|
+      results << article if count < 3
+      count += 1
+    end
+    results
+  end
+  
+  def display_row2_elements(articles)
+    count = 1
+    results = []
+    articles.each do |article|
+      results << article if count >= 3 && count < 5
+      count += 1
+    end
+    results
+  end
 end
