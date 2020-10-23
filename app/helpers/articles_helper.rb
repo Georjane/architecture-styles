96d32b7bbfcc553a982ede2bc343f9bbedf50c2b
Georjane <pzgeorjane@gmail.com>
2020-10-23 11:32:28 +0000

    Remove logic for view and add section and article helper methods

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,13 @@
 module ArticlesHelper
+  def vote_btn(article)
+    btn = ''
+    pre_vote = article.votes.find { |vote| vote.user_id == current_user.id}
+    if pre_vote
+      btn << (button_to 'Unvote', article_vote_path(article, article.votes.find { |vote| vote.user_id == current_user.id}), method: :delete)
+    else
+      btn << (button_to 'vote', article_votes_path(article), method: :post)
+    end
+    btn.html_safe
+    
+  end
 end
